aa0f584399acda2856dac1dfaf127f45cfc2c682
Denys Malieiev <denysmalieiev@gmail.com>
2023-12-31 02:51:14 +0530

    Header Redesign

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { carRental_Get_All_Cars, carRental_Admin_All_Offices_Load, clearError } from '../../utils/actions/CarsAction.js';
 import { carRental_Sign_Out } from '../../utils/actions/UserAction.js';
 import headerCSS from './css/header.module.css';
@@ -9,72 +9,67 @@ const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate()
 
-  const { isAuthenticated, role } = useSelector(state=> state.auth);
-  const { cars, loading, error } = useSelector(state=> state.cars);
+  const { isAuthenticated, role } = useSelector(state => state.auth);
+  const { cars, loading, error } = useSelector(state => state.cars);
 
-  const handleLogout =(e) =>{
+  const handleLogout = (e) => {
     e.preventDefault();
     alert('Logout')
     dispatch(carRental_Sign_Out)
     navigate('/')
   }
 
-  useEffect(()=>{
-    if(error){
+  useEffect(() => {
+    if (error) {
       dispatch(clearError)
     }
     // if(!cars || Object.keys(cars).length===0){
     //   dispatch(carRental_Get_All_Cars)
     // }
-    if(isAuthenticated && role){
-      if(role==='admin'){
+    if (isAuthenticated && role) {
+      if (role === 'admin') {
         navigate('/admin/dashboard')
       }
     }
   }, [dispatch, isAuthenticated, error, role])
 
   return (
-    <>
-      <div className={headerCSS.carRentalNevBar}>
+    <div>
+      <div className={headerCSS.carRentalNavBar}>
         <div className={headerCSS.navBarWebName}>
           <p><Link to='/'>Car Rental</Link></p>
         </div>
         <div className={headerCSS.navBarWebNavigation}>
-          <div className={headerCSS.navBarWebNavigationLeft}>
-            <ul>
-              { isAuthenticated && !loading
-                ? <> 
-                  { role==='admin'
-                    ? <>
-                        <li><h3><Link to='/cars'>Cars</Link></h3></li>
-                        <li><h3><Link to='/admin/dashboard'>Admin</Link></h3></li>
-                        <li><h3><Link to='/admin/office/details'>Offices</Link></h3></li>
-                      </>
-                    : <></>
-                  }
-                  </>
-                : <></>
-              }
-            </ul>
-          </div>
-          <div className={headerCSS.navBarWebNavigationRight}>
-            <ul>
-              { isAuthenticated
-                ? <>
-                    <li><h3><Link to='/cars/booking/history'>Bookings</Link></h3></li>
-                    <li><h3><Link to='/user/profile'>Profile</Link></h3></li>
-                    <li><h3 style={{color: 'white'}} onClick={handleLogout}><Link>Logout</Link></h3></li>
-                  </>
-                : <>
-                    <li><h3><Link to='/user/signin'>SignIn</Link></h3></li>
-                    <li><h3><Link to='/user/signup'>SignUp</Link></h3></li>
-                  </>
-              }
-              </ul>
+          <div className={headerCSS.navBarGeneralOption}>
+            <p><Link to='/cars'>Cars</Link></p>
+            <p><Link to='/user/signin'>Sign In</Link></p>
+            <p><Link to='/user/signup'>Sign Up</Link></p>
+            {isAuthenticated && !loading ?
+              <>
+                <p><Link>Order</Link></p>
+                <p>Profile</p>
+                <p><Link>Logout</Link></p>
+              </>
+              : <></>
+            }
+
           </div>
         </div>
       </div>
-    </>
+      {isAuthenticated && !loading ?
+        <>
+          <div className={headerCSS.navBarAuthOption}>
+            <p><Link to='/cars/booking/history'>Bookings</Link></p>
+            <p><Link to='/user/profile'>Profile</Link></p>
+          </div>
+        </>
+        : <></>
+      }
+
+      <div className={headerCSS.navBarOfferBox}>
+        <p>15% Discount this spring season.</p>
+      </div>
+    </div>
   )
 }
 
