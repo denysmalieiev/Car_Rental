ddae237dbbee9deaf249bb8d683e3904b444a396
Denys Malieiev <denysmalieiev@gmail.com>
2023-12-31 01:33:54 +0530

    Code changes

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -1,6 +1,6 @@
 import {legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {thunk} from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { userAuthReducer, carRental_User_Profile_Load, carRental_User_Profile_Update, carRental_profile_Password_Update, carRental_Admin_All_Users_Load, carRental_Admin_Single_User_Load, carRental_User_Cart_Car_Book_Reducer } from '../utils/reducers/UserReducers.js';
 import { allCarReducer, singleCarReducer, adminNewOfficeReducer, adminAllOfficesReducer, adminSingleOfficeReducer } from '../utils/reducers/CarsReducers.js';
 import { carRental_User_Single_Order, carRental_User_All_Orders, carRental_Admin_All_Users_Orders } from '../utils/reducers/OrderReducer.js';
@@ -19,7 +19,6 @@ const reducer = combineReducers({
     adminOffice: adminNewOfficeReducer,
     office: adminSingleOfficeReducer,
     offices: adminAllOfficesReducer,
-    // userRole: 
     userSingleOrder: carRental_User_Single_Order,
     userOrders: carRental_User_All_Orders,
     adminOrders: carRental_Admin_All_Users_Orders
